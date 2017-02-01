9baf0a6d1206cdcdd457939d61c5399d9f47162c
jamieschirra <jamieschirra@yahoo.com>
2017-02-01 18:53:11 -0500

    re-name the routes for the CustomerCareController

--- Controllers/CustomerCare.cs
+++ Controllers/CustomerCare.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 
+[Route("claim")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
     }
-    public IActionResult Claims()
+    [Route("fileclaim")]
+    public IActionResult FileClaim()
     {
-        return Ok("This is for the claims from the CustomerCareController");
+        return Ok("This is for the file claim from the CustomerCareController");
     }
-
+    [Route("myclaims")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("This is a claim history from the CustomerCareController");
+    }
 }
